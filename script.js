ad88c9bff6c3085c17bf3e5a0994261b5555a0c9
KHESNER <136658026+NERDZIM@users.noreply.github.com>
2024-10-04 19:02:48 +0000

    botton

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ form.addEventListener('submit', (event) => {
     const jogo = document.getElementById('jogo').value;
     const descricao = document.getElementById('descricao').value;
 
-    // Aqui você implementaria a lógica para enviar os dados para um servidor ou realizar outra ação, como enviar um email
+    
     console.log('Dados da denúncia:', nome, idade, jogo, descricao);
 
-    // Exibir uma mensagem de sucesso (ajuste conforme necessário)
     alert('Denúncia enviada com sucesso! Nossa equipe entrará em contato.');
 });
 
