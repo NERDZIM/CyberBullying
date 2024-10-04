88b6f556ab8c9b410ba78105d1588b430843f763
KHESNER <136658026+NERDZIM@users.noreply.github.com>
2024-10-04 18:39:18 +0000

    qa

--- script.js
+++ script.js
@@ -52,3 +52,14 @@ User.create({
 .catch(err => {
   console.error(err);
 });
+
+function toggleMenu() {
+  const menuContent = document.getElementById('menu-content');
+  menuContent.style.display = (menuContent.style.display === 'block') ? 'none' : 'block';
+}
+
+
+function toggleTheme() {
+  const body = document.body;
+  body.classList.toggle('dark-theme');
+}
